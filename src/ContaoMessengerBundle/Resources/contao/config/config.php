c2f43b465b05636b269dfc15c994cd16281f2ee1
hh-com <info@harald-huber.com>
2019-02-07 20:50:38 +0000

    10

--- src/ContaoMessengerBundle/Resources/contao/config/config.php
+++ src/ContaoMessengerBundle/Resources/contao/config/config.php
@@ -1,6 +1,15 @@
 <?php
 
 
+    /**
+     * Back end modules
+     */
+    $GLOBALS['BE_MOD']['content']['pm'] = array
+    (
+        'tables'			=> array('tl_messenger'),
+        'icon'				=> 'system/modules/pm/html/icon.gif',
+    );
+
     $GLOBALS['FE_MOD']['user']['cmessenger'] = 'ContaoMessengerBundle\MessengerModule';
 
 ?>
